f4c70dd64dffc6e98cde7142823f47cc9c9e7e39
GJSayers <gemma.sayers1@btinternet.com>
2022-06-27 16:35:15 +0000

    Add Profile page update functionality

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -15,6 +15,7 @@ import Comment from "../comments/Comment";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 
 
@@ -45,7 +46,8 @@ function PostPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+        <PopularProfiles />
+      
         <Post {...post.results[0]} setPosts={setPost} PostPage/>
         <Container className={appStyles.Content}>
         {currentUser ? (
