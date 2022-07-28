27453d6c4a1ffcce2076f7557f4853c2b49dfaca
GJSayers <gemma.sayers1@btinternet.com>
2022-07-28 20:21:18 +0000

    Test user contexts, error cleanup part 1

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -1,53 +1,48 @@
-import { render, screen, fireEvent } from "@testing-library/react"
-import {BrowserRouter as Router} from 'react-router-dom'
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
-import NavBar from "../NavBar"
+import NavBar from "../NavBar";
 
-test('Renders NavBar', () => {
-    render(
-        <Router>
-            <NavBar />
-        </Router>
-     );
+test("renders NavBar", () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
 
-    //  screen.debug();
-    const signInLink = screen.getByRole('link', {name: 'Sign in'});
-    expect(signInLink).toBeInTheDocument();
-
+  // screen.debug();
+  const signInLink = screen.getByRole("link", { name: "Sign in" });
+  expect(signInLink).toBeInTheDocument();
 });
 
-test('Renders Link to the profile for a logged in user', async () => {
-    render(
-        <Router>
-            <CurrentUserProvider>
-                <NavBar />
-            </CurrentUserProvider>
-        </Router>
-     );
+test("renders link to the user profile for a logged in user", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
 
-    const profileAvatar = await screen.findByText('Profile');
-    expect(profileAvatar).toBeInTheDocument();
-
+  const profileAvatar = await screen.findByText("Profile");
+  expect(profileAvatar).toBeInTheDocument();
 });
 
-test('Renders signin and signup buttons again on logout', async () => {
-    render(
-        <Router>
-            <CurrentUserProvider>
-                <NavBar />
-            </CurrentUserProvider>
-        </Router>
-     );
+test("renders Sign in and Sign up buttons again on log out", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
 
-    const signOutLink = await screen.findByRole('link', {name: 'Sign out'});
-    fireEvent.click(signOutLink);
+  const signOutLink = await screen.findByRole("link", { name: "Sign out" });
+  fireEvent.click(signOutLink);
 
-    const signInLink = await screen.findByRole('link', {name: 'Sign in'});
+  const signInLink = await screen.findByRole("link", { name: "Sign in" });
+  const signUpLink = await screen.findByRole("link", { name: "Sign up" });
 
-    const signUpLink = await screen.findByRole('link', {name: 'Sign up'});
-
-    
-    expect(signInLink).toBeInTheDocument();
-    expect(signUpLink).toBeInTheDocument();
-
+  expect(signInLink).toBeInTheDocument();
+  expect(signUpLink).toBeInTheDocument();
 });
